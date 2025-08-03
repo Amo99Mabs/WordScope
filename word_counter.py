56db7acf307fddccb3eacbc00b1b6d266ffe06ec
Amo99Mabs <174100269+Amo99Mabs@users.noreply.github.com>
2025-08-03 19:38:44 +0200

    Added word frequency counter for sentence preprocessing
    - Removed punctuation and normalized case
    - Split sentence into words
    - Counted word occurrences using dictionary

---
+++ word_counter.py
@@ -0,0 +1,16 @@
+sentence = """A horse is a horse, of course, of course,
+And no one can talk to a horse of course,
+That is, of course, unless the horse is the famous """
+
+
+
+sentence = sentence.replace(',', '').lower().split()
+wc = {}
+
+for word in sentence:
+    if word in wc.keys():
+        wc[word] +=1
+    else: 
+        wc[word] = 1
+
+print(wc)
